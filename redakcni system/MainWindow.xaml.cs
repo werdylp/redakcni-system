9b2c80efed784fc4457269f33487689be562b17a
werdylp <werca.cerna@gmail.com>
2023-01-05 09:20:41 +0100

    Přidejte soubory projektu.

---
+++ redakcni system/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace redakcni_system
+
+
+{
+    /// <summary>  
+    /// Interaction logic for MainWindow.xaml  
+    /// </summary>  
+    /// 
+
+    public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        newArticle p = new newArticle();
+        p.Show();
+    }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            seznam p = new seznam();
+            p.Show();
+        }
+
+
+
+    }  
+}
---
+++ redakcni system/newArticle.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace redakcni_system
+{
+    /// <summary>
+    /// Interakční logika pro newArticle.xaml
+    /// </summary>
+    public partial class newArticle : Window
+    {
+        
+        public newArticle()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+            
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
